cb1ac30e6d559cd259c2d4b92f54ef5cf2f9e6f4
sailikhitha369 <sailikitha48757@gmail.com>
2024-06-11 13:40:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "./actions";
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const [todoTitle, setTodoTitle] = useState("");
+  const handleTodo = () => {
+    if (todoTitle.trim()) {
+      const newTodo = {
+        title: todoTitle,
+      };
+      dispatch(addTodo(newTodo));
+      setTodoTitle("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 w-50">
+      <h3 className="text-primary text-center">Todo app using redux react</h3>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={() => handleTodo()}>
+          Add
+        </button>
+      </div>
+      <TodoList />
     </div>
   );
 }
